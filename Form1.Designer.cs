c21db243fd06361971c2faba0982cc8bff1d87f0
OstapchukAB <electron.anton@gmail.com>
2019-10-13 18:48:17 +0300

    сделал два таймера, рандомную генерацию чисел, выбор в листбоксе ограничителя числа

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(51, 93);
+            this.textBox1.Location = new System.Drawing.Point(56, 45);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(165, 80);
+            this.textBox1.Size = new System.Drawing.Size(219, 98);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
@@ -49,18 +56,21 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(315, 90);
+            this.textBox2.Location = new System.Drawing.Point(408, 42);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(165, 80);
+            this.textBox2.Size = new System.Drawing.Size(219, 98);
             this.textBox2.TabIndex = 1;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(224, 97);
+            this.label1.Location = new System.Drawing.Point(287, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 73);
+            this.label1.Size = new System.Drawing.Size(113, 90);
             this.label1.TabIndex = 3;
             this.label1.Text = "+";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -69,18 +79,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(510, 93);
+            this.label2.Location = new System.Drawing.Point(668, 45);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 73);
+            this.label2.Size = new System.Drawing.Size(87, 91);
             this.label2.TabIndex = 4;
             this.label2.Text = "=";
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(607, 90);
+            this.textBox3.Location = new System.Drawing.Point(797, 42);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(165, 80);
+            this.textBox3.Size = new System.Drawing.Size(219, 98);
             this.textBox3.TabIndex = 5;
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckEnter);
@@ -88,24 +100,67 @@
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(111, 268);
+            this.label3.Location = new System.Drawing.Point(314, 351);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(570, 106);
+            this.label3.Size = new System.Drawing.Size(760, 130);
             this.label3.TabIndex = 6;
             this.label3.Text = "ffff";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 91;
+            this.listBox1.Location = new System.Drawing.Point(50, 365);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(219, 95);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(50, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(219, 121);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Выберите максимальное число:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(410, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(606, 91);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 424);
+            this.ClientSize = new System.Drawing.Size(1131, 522);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Проверка счета";
@@ -123,6 +178,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,42 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Calculator
 {
+
+
+
     public partial class Form1 : Form
     {
         int result;
+        Random random1;
+        Random random2;
+        int rnd;
+        int maxDigit;
+        int counter;
+        int counter2;
         public Form1()
         {
             InitializeComponent();
         }
 
-        
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            label3.Text = "";
-        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            label5.Text = "";
             label3.Text = "";
+            
+            for (int i = 10; i<=100;i++) {
+                listBox1.Items.Add(i);
+            }
+            maxDigit = 10;// new Random().Next(10, 100);
+            listBox1.SelectedItem =maxDigit;
+            Method2();
+            counter = 0;
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            // InitializeTimer();
         }
 
         private void CheckEnter(object sender, System.Windows.Forms.KeyPressEventArgs e)
         {
             //Console.WriteLine(e.KeyChar);
-            
+
             if (e.KeyChar == (char)13)
             {
-                
+
                 this.Method();
 
             }
@@ -44,14 +56,22 @@ namespace Calculator
 
         }
 
-       
 
-        void Method() {
+
+        void Method()
+        {
             result = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text);
             if (Convert.ToInt32(textBox3.Text) == result)
             {
                 label3.Text = "Правильно!";
                 this.label3.ForeColor = System.Drawing.Color.Green;
+                timer1.Stop();
+                timer1.Enabled = false;
+                counter = 0;
+                counter2 = 0;
+                timer2.Enabled = true;
+                timer2.Start();
+                
             }
             else
             {
@@ -63,12 +83,80 @@ namespace Calculator
 
         }
 
-        private void textBox1_Click(object sender, EventArgs e)
+        void Method2()
         {
-            textBox1.Text = "";
-            textBox2.Text = "";
             textBox3.Text = "";
             label3.Text = "";
+            maxDigit = Convert.ToInt32(listBox1.SelectedItem);
+            random1 = new Random();
+            rnd = random1.Next(1, maxDigit+1);
+            textBox1.Text = Convert.ToString(rnd);
+            random2 = new Random();
+            rnd = random1.Next(1, maxDigit+1);
+            textBox2.Text = Convert.ToString(rnd);
+            
+
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            Method3();
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            Method3();
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            Method3();
+        }
+
+
+        private void InitializeTimer()
+        {
+            // Run this procedure in an appropriate event.  
+            counter = 0;
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            counter2 = 0;
+            timer2.Interval = 1000;
+            timer2.Enabled = false;
+            // Hook up timer's tick event handler.  
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter = counter + 1;
+            label5.Text = $"Время от начала задачи:\n{counter} сек.";
+        }
+
+        void Method3() {
+            Method2();
+            textBox3.Focus();
+            counter = 0;
+           
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            timer1.Start();
+            //InitializeTimer();
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            counter2 = counter2 + 1;
+            if (counter2 > 20) {
+                label5.Text = $"Новая задача!";
+                timer2.Stop();
+                timer2.Enabled = false;
+                counter2 = 0;
+                Method3();
+            }
         }
     }
 }
